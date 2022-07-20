d5e6b9303d4ea47484932b9cdc823db2f822fc05
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-07-20 17:37:47 +0800

    :white_check_mark: fixed tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,5 @@
-from lamin_logger import colors, logger
+from lamin_logger import logger
 
 
 def test_logger():
     assert logger.level("INFO").name == "INFO"
-    assert colors.green("text") == "\x1b[1;92mtest\x1b[0m"
