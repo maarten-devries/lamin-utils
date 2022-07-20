eee7e2fd04d1b1fd321adf0a8f8303ba7a3af27b
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-07-20 17:40:47 +0800

    :bug: fixed docstring

--- lamin_logger/__init__.py
+++ lamin_logger/__init__.py
@@ -9,8 +9,8 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   example_function
-   ExampleClass
+   logger
+   colors
 """
 
 __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
