326328ea3d0bb4ff334a988004eafcdbd6cb612d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-07-20 17:20:14 +0800

    :sparkles: Added logger and colors

--- lamin_logger/__init__.py
+++ lamin_logger/__init__.py
@@ -15,4 +15,4 @@ This is the complete API reference:
 
 __version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
 
-from ._core import ExampleClass, example_function  # noqa
+from ._core import colors, logger  # noqa
