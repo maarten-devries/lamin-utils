dbe4e2088c69090545ebfab08cebc489f8a586c2
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-01-12 05:04:15 +0100

    🔥 Manually provide versions for py_version_warning (#13)
    * :fire: Manually provide versions for py_version_warning
    
    * :lipstick: prettfy warning messge

--- lamin_logger/_python_version.py
+++ lamin_logger/_python_version.py
@@ -1,9 +1,6 @@
 # Raise warnings for python versions that are not tested
 import platform
-from pathlib import Path
-from typing import Optional
 
-import yaml  # type: ignore
 from packaging import version
 
 from ._core import logger
@@ -11,31 +8,14 @@ from ._core import logger
 py_version = version.parse(platform.python_version())
 
 
-def py_version_warning(pkg_filepath: Optional[str]):
-    if pkg_filepath is not None:
-        with open(
-            Path(pkg_filepath).parent.parent / ".github/workflows/build.yml", "r"
-        ) as f:
-            versions = (
-                yaml.safe_load(f)
-                .get("jobs")
-                .get("build")
-                .get("strategy")
-                .get("matrix")
-                .get("python-version")
-            )
-        min_v = versions[0]
-        max_v = versions[-1]
-    else:
-        min_v = "3.7"
-        max_v = "3.11"
-
+def py_version_warning(min_v: str, max_v: str):
+    """Print warning for python versions that are not tested in CI."""
     max_v_plus_1 = (
         ".".join(max_v.split(".")[:-1]) + "." + str(int(max_v.split(".")[-1]) + 1)
     )
 
     if py_version >= version.parse(max_v_plus_1) or py_version < version.parse(min_v):
         logger.warning(
-            f"Python versions < {min_v} or >= {max_v_plus_1} are currently not tested,"
-            " use at your own risk."
+            f"You are running {py_version}\nPython versions < {min_v} or >="
+            f" {max_v_plus_1} are currently not tested, use at your own risk."
         )
