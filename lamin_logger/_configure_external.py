9eca5d4aa3ebd63c4e8a9c3b175d457c7a5d6502
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-06 07:46:17 +0100

    🔇 Suppress "Found credentials..." logging in aiobotocore (#15)
    * :mute: Suppress Found credentials logging in aiobotocore
    
    * :art: simplify

--- lamin_logger/_configure_external.py
+++ lamin_logger/_configure_external.py
@@ -3,12 +3,10 @@ import logging
 try:
     import boto3
 
-    boto3.set_stream_logger("boto3", logging.WARNING)
-    import botocore  # noqa
-
-    logger = logging.getLogger("botocore")
-    logger.setLevel(logging.WARNING)
-    logger = logging.getLogger("botocore.credentials")
-    logger.setLevel(logging.WARNING)
+    # this gets logged 6 times
+    boto3.set_stream_logger(name="botocore.credentials", level=logging.WARNING)
+    # the 7th logging message of credentials came from aiobotocore
+    boto3.set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
+    boto3.set_stream_logger(name="urllib3.connectionpool", level=logging.WARNING)
 except ImportError:
     pass
