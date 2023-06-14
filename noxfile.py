83cab8a95eb78dd568dc9b3923c8fe0a48550f8a
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 19:38:03 +0200

    🚚 Temporarily added lookup (#20)
    * :sparkles: Temporarily added lookup
    
    * :green_heart: Fix
    
    * :green_heart: Fix
    
    * :green_heart: Fix
    
    * :green_heart: Fix
    
    * :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -1,28 +1,16 @@
-from pathlib import Path
-
+import laminci
 import nox
 
-nox.options.reuse_existing_virtualenvs = True
+nox.options.default_venv_backend = "none"
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def lint(session: nox.Session) -> None:
-    session.install("pre-commit")
-    session.run("pre-commit", "install")
-    session.run("pre-commit", "run", "--all-files")
+    laminci.nox.run_pre_commit(session)
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def build(session):
-    session.install(".[dev,test]")
-    session.run(
-        "pytest",
-        "-s",
-        "--cov=lamin_logger",
-        "--cov-append",
-        "--cov-report=term-missing",
-    )
-    session.run("coverage", "xml")
-    prefix = "." if Path("./lndocs").exists() else ".."
-    session.install(f"{prefix}/lndocs")
-    session.run("lndocs")
+    session.run(*"pip install .[dev,test]".split())
+    laminci.nox.run_pytest(session)
+    laminci.nox.build_docs(session)
